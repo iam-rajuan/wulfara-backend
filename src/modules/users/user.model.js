351f6174b7bbb5770c0d8ed3162fea7da559f050
mdmehedihasan-dev <mdmehedihasandev0@gmail.com>
2026-07-26 19:15:01 +0600

    user modal update

--- src/modules/users/user.model.js
+++ src/modules/users/user.model.js
@@ -30,6 +30,11 @@ const userSchema = new mongoose.Schema({
     type: Boolean,
     default: false
   },
+  status: {
+    type: String,
+    enum: ['Active', 'Suspended'],
+    default: 'Active'
+  },
   verifyCode: String,
   resetPasswordToken: String,
   resetPasswordExpire: Date,

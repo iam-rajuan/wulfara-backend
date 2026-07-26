70f8565c0b1520c35dd2ba5091a13e31eac75549
mdmehedihasan-dev <mdmehedihasandev0@gmail.com>
2026-07-26 20:32:07 +0600

    subscription

--- src/modules/subscriptions/pricingPlan.model.js
+++ src/modules/subscriptions/pricingPlan.model.js
@@ -1,17 +1,54 @@
 const mongoose = require('mongoose');
 
 const pricingPlanSchema = new mongoose.Schema({
+  internalName: {
+    type: String,
+    required: [true, 'Please add an internal plan name']
+  },
   name: {
     type: String,
-    required: [true, 'Please add a plan name']
+    required: [true, 'Please add a display plan name']
+  },
+  slug: {
+    type: String,
+    required: [true, 'Please add a slug'],
+    unique: true
+  },
+  description: {
+    type: String,
+    default: ''
+  },
+  badgeText: {
+    type: String,
+    default: ''
+  },
+  accentColor: {
+    type: String,
+    default: '#D4AF37'
   },
   price: {
     type: Number,
     required: [true, 'Please add a price']
   },
+  billingCycle: {
+    type: String,
+    default: 'Annual (Paid Upfront)'
+  },
+  taxCategory: {
+    type: String,
+    default: 'Standard Digital Service'
+  },
+  allowCoupons: {
+    type: Boolean,
+    default: true
+  },
+  autoRenewal: {
+    type: Boolean,
+    default: true
+  },
   features: {
     type: [String],
-    required: true
+    default: []
   },
   isActive: {
     type: Boolean,

84d087f8c5d4c3761803e860952d7635f75fa8f0
mdmehedihasan-dev <mdmehedihasandev0@gmail.com>
2026-07-28 18:26:17 +0600

    backend image size increse

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Basic route for health check
 app.get('/health', (req, res) => {
